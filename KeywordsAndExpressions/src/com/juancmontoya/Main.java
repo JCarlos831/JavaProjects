374525b8dc5edc6243235765f4b6338cb13884e1
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-11-25 18:37:25 -0500

    Indentation, keywords, whitespaces, expressions, statements, code blocks, and methods

---
+++ KeywordsAndExpressions/src/com/juancmontoya/Main.java
@@ -0,0 +1,34 @@
+package com.juancmontoya;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Keywords
+        //
+        // abstract,	continue,	for,	new,	switch,
+        // assert***,	default,	goto*,	package,	synchronized,
+        // boolean,	do,	if,	private,	this,
+        // break,	double,	implements,	protected,	throw,
+        // byte,	else,	import,	public,	throws,
+        // case,	enum****,	instanceof,	return,	transient,
+        // catch,	extends,	int,	short,	try,
+        // char,	final,	interface,	static,	void,
+        // class,	finally,	long,	strictfp**,	volatile,
+        // const*,	float,	native,	super,	while
+        // *	 	not used
+        // **	 	added in 1.2
+        // ***	 	added in 1.4
+        // ****	 	added in 5.0
+
+
+        // Expressions
+
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+
+        int highscore = 50;
+        if (highscore == 50) {
+            System.out.println("This is an expression.");
+        }
+    }
+}
