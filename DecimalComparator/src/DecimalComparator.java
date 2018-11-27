da7a89d0a41a71ef6fd2b7514d9deb7a47179c78
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-11-26 23:11:52 -0500

    Refactored with one line solution converting double to int

--- DecimalComparator/src/DecimalComparator.java
+++ DecimalComparator/src/DecimalComparator.java
@@ -1,6 +1,3 @@
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
-
 public class DecimalComparator {
     public static void main(String[] args) {
         areEqualByThreeDecimalPlaces(-3.1756, -3.175);
@@ -9,38 +6,44 @@ public class DecimalComparator {
         areEqualByThreeDecimalPlaces(3.0, 3.0);
     }
 
+//    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+//        String num1AsString = "";
+//        String num2AsString = "";
+//        if (num1 < 0) {
+//            DecimalFormat df = new DecimalFormat("#.000");
+//            df.setRoundingMode(RoundingMode.CEILING);
+//            num1AsString = df.format(num1);
+//        }
+//        else {
+//            DecimalFormat df = new DecimalFormat("#.000");
+//            df.setRoundingMode(RoundingMode.FLOOR);
+//            num1AsString = df.format(num1);
+//        }
+//
+//        if (num2 < 0) {
+//            DecimalFormat df = new DecimalFormat("#.000");
+//            df.setRoundingMode(RoundingMode.CEILING);
+//            num2AsString = df.format(num2);
+//        }
+//        else {
+//            DecimalFormat df = new DecimalFormat("#.000");
+//            df.setRoundingMode(RoundingMode.FLOOR);
+//            num2AsString = df.format(num2);
+//        }
+//
+//        System.out.println(num1AsString);
+//        System.out.println(num2AsString);
+//
+//        if (num1AsString.equals(num2AsString)) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+
     public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
-        String num1AsString = "";
-        String num2AsString = "";
-        if (num1 < 0) {
-            DecimalFormat df = new DecimalFormat("#.000");
-            df.setRoundingMode(RoundingMode.CEILING);
-            num1AsString = df.format(num1);
-        }
-        else {
-            DecimalFormat df = new DecimalFormat("#.000");
-            df.setRoundingMode(RoundingMode.FLOOR);
-            num1AsString = df.format(num1);
-        }
-
-        if (num2 < 0) {
-            DecimalFormat df = new DecimalFormat("#.000");
-            df.setRoundingMode(RoundingMode.CEILING);
-            num2AsString = df.format(num2);
-        }
-        else {
-            DecimalFormat df = new DecimalFormat("#.000");
-            df.setRoundingMode(RoundingMode.FLOOR);
-            num2AsString = df.format(num2);
-        }
-
-        System.out.println(num1AsString);
-        System.out.println(num2AsString);
-
-        if (num1AsString.equals(num2AsString)) {
-            return true;
-        } else {
-            return false;
-        }
+        return ((int) (num1 * 1000) == (int) (num2 * 1000));
     }
+
 }
+
