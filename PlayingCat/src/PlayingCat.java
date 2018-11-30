89facb47d08716714dd5ae1b9a60d04a59e1b2fd
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-11-29 19:38:00 -0500

    Used conditionals and operators to solve coding challenge

---
+++ PlayingCat/src/PlayingCat.java
@@ -0,0 +1,16 @@
+public class PlayingCat {
+    public static void main(String[] args) {
+        isCatPlaying(true, 10);
+        isCatPlaying(false, 36);
+        isCatPlaying(false, 35);
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer && temperature >= 25 && temperature <= 45)
+            return true;
+        else if (!summer && temperature >= 25 && temperature <= 35)
+            return true;
+        else
+            return false;
+    }
+}
