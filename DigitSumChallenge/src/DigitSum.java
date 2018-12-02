9d08a9a22f0e352924ef3c41f7a9fbc23702e373
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-12-01 21:21:17 -0500

    Solved using if statement and while loop

--- DigitSumChallenge/src/DigitSum.java
+++ DigitSumChallenge/src/DigitSum.java
@@ -1,6 +1,6 @@
 public class DigitSum {
     public static void main(String[] args) {
-        System.out.println(sumDigits(12514567));
+        System.out.println(sumDigits(12514567  ));
     }
 
     public static int sumDigits(int number) {
