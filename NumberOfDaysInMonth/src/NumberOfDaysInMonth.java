94e04a86348421a0eaffa7ea4edde80ab9611da4
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-12-01 21:17:34 -0500

    typo correction

--- NumberOfDaysInMonth/src/NumberOfDaysInMonth.java
+++ NumberOfDaysInMonth/src/NumberOfDaysInMonth.java
@@ -24,7 +24,7 @@ public class NumberOfDaysInMonth {
         boolean leapYear = isLeapYear(year);
 
         switch (month) {
-            case 1: case 3: casse 5: case 7: case 8: case 10: case 12:
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
                 return 31;
             case 2:
                 if (leapYear)
