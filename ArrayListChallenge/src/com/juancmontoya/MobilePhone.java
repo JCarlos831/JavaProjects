245a3cdd561943b426f6f83ea223c830614e5dce
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-12-16 13:13:34 -0500

    Mobile phone contacts CRUD completed

--- ArrayListChallenge/src/com/juancmontoya/MobilePhone.java
+++ ArrayListChallenge/src/com/juancmontoya/MobilePhone.java
@@ -26,6 +26,9 @@ public class MobilePhone {
         if (foundPosition < 0) {
             System.out.println(oldContact.getName() + " was not found.");
             return false;
+        } else if (findContact(newContact.getName()) != -1) {
+            System.out.println("Contact with name " + newContact.getName() + " already exists. Update was not successful.");
+            return false;
         }
 
         this.myContacts.set(foundPosition, newContact);
@@ -60,7 +63,24 @@ public class MobilePhone {
 
     public String queryContact(Contact contact) {
         if (findContact(contact) >= 0) {
-            return contact.getName()
+            return contact.getName();
+        }
+        return null;
+    }
+
+    public Contact queryContact(String name) {
+        int position = findContact(name);
+        if (position >= 0) {
+            return this.myContacts.get(position);
+        }
+        return null;
+    }
+
+    public void printContacts() {
+        System.out.println("Contact List");
+        for (int i = 0; i < this.myContacts.size(); i++) {
+            System.out.println((i + 1) + "." + this.myContacts.get(i).getName() + " -> " +
+                                               this.myContacts.get(i).getPhoneNumber());
         }
     }
 }
